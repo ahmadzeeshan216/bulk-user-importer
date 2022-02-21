0e0484cecf9f024d5b7c2c368fe51d8122aa6b31
Ahmad Zeeshan <ahmadzeeshan216@gmail.com>
2022-02-21 13:03:52 -0800

    FlashMessages: Created new partial for flash messages and assigned respective classes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def flash_message_classes(type)
+    case type.to_sym
+    when :error
+      'alert alert-danger'
+    when :notice
+      'alert alert-info'
+    else
+      ''
+    end
+  end
 end
