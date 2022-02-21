6ed5cc5e3ce736e2601133aec7bacf9189ac016c
Ahmad Zeeshan <ahmadzeeshan216@gmail.com>
2022-02-21 10:35:14 -0800

    Routes: Added routes for bulk user import enpoints

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :users, only: [] do
+    collection do
+      get :new_import
+      post :import
+    end
+  end
+
+  root to: 'users#new_import'
 end
