fb8acd6757a22edd5d939f5a40db2d7c155d221b
Ahmad Zeeshan <ahmadzeeshan216@gmail.com>
2022-02-21 13:36:02 -0800

    UsersModule: UI improvements

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,2 @@
 module UsersHelper
-  def pretty_creation_message(user)
-    if user.errors.any?
-      user.errors.full_messages.to_sentence
-    else
-      t('user.success.creation', name: user.name)
-    end
-  end
 end
